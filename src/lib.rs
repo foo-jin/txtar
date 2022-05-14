9296b56e7a7ba3cb06d59d6a74fcc90f631b1ed9
Frank de Jong <f.jin@protonmail.com>
2022-05-14 21:37:11 +0200

    [WIP] Change internal representation to use bytes instead of `str`

--- src/lib.rs
+++ src/lib.rs
@@ -11,24 +11,55 @@ use std::{
 mod error;
 
 const NEWLINE_MARKER: &str = "\n-- ";
-const NEWLINE_MARKER_END: &str = " --\n";
+const MARKER: &str = "-- ";
+const MARKER_END: &str = " --";
 
 pub use error::MaterializeError;
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct Archive<'a> {
-    comment: &'a str,
+    // internal invariant:
+    // comment is fix_newlined
+    comment: Cow<'a, [u8]>,
     files: Vec<File<'a>>,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct File<'a> {
-    name: &'a str,
-    data: &'a str,
+    name: &'a [u8],
+    // internal invariant:
+    // data is fix_newlined
+    data: Cow<'a, [u8]>,
+}
+
+impl<'a> File<'a> {
+    pub fn new(name: &'a str, data: &'a str) -> File<'a> {
+        let mut data = Cow::Borrowed(data.as_bytes());
+        fix_newline(&mut data);
+
+        File {
+            name: name.as_bytes(),
+            data,
+        }
+    }
 }
 
 impl<'a> Archive<'a> {
+    fn new(comment: &'a str, files: Vec<File<'a>>) -> Archive<'a> {
+        let mut comment = Cow::Borrowed(comment.as_bytes());
+        fix_newline(&mut comment);
+
+        Archive { comment, files }
+    }
+
     /// Serialize the archive as txtar into the I/O stream.
     pub fn to_writer<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        write!(writer, "{}", self)
+        writer.write_all(&self.comment)?;
+        for File { name, data } in &self.files {
+            writer.write_all(name)?;
+            writer.write_all(data)?;
+        }
+        Ok(())
     }
 
     /// Writes each file in this archive to the directory at the given
@@ -43,7 +74,8 @@ impl<'a> Archive<'a> {
     pub fn materialize<P: AsRef<Path>>(&self, path: P) -> Result<(), MaterializeError> {
         let path = path.as_ref();
         for File { name, data } in &self.files {
-            let name_path = PathBuf::from(path_clean::clean(name));
+            // this is disgusting, TODO
+            let name_path = PathBuf::from(path_clean::clean(&String::from_utf8_lossy(name)));
             if name_path.starts_with("../") || name_path.is_absolute() {
                 return Err(MaterializeError::DirEscape(
                     name_path.to_string_lossy().to_string(),
@@ -56,14 +88,12 @@ impl<'a> Archive<'a> {
                 fs::create_dir_all(p)?;
             }
 
-            let mut data = Cow::Borrowed(*data);
-            fix_newline(&mut data);
             let mut file = fs::File::options()
                 .write(true)
                 .create_new(true)
                 .open(path)?;
             let mut w = BufWriter::new(&mut file);
-            w.write_all(data.as_bytes())?;
+            w.write_all(data)?;
         }
 
         Ok(())
@@ -78,52 +108,58 @@ impl<'a> From<&'a str> for Archive<'a> {
         while !name.is_empty() {
             let (data, next_name, rest) = split_file_markers(s);
 
-            let file = File { name, data };
+            let file = File::new(name, data);
             files.push(file);
 
             name = next_name;
             s = rest;
         }
 
-        Archive { comment, files }
+        Archive::new(comment, files)
     }
 }
 
 fn split_file_markers(s: &str) -> (&str, &str, &str) {
-    let (prefix, rest) = match s.split_once(NEWLINE_MARKER) {
-        Some(split) => split,
+    let (prefix, rest) = match s.find(NEWLINE_MARKER) {
         None => return (s, "", ""),
+        Some(offset) => s.split_at(offset + 1),
+    };
+    debug_assert!(rest.starts_with(MARKER));
+
+    let (name, postfix) = match rest.split_once('\n') {
+        None => return (s, "", ""),
+        Some((n, pf)) => (n, pf),
     };
 
-    let (name, postfix) = match rest.split_once(NEWLINE_MARKER_END) {
-        Some(split) => split,
-        None => return (s, "", ""),
-    };
+    let name = name.trim_end_matches('\r');
+    debug_assert!(name.ends_with(MARKER_END));
 
+    let name = name
+        .strip_prefix(MARKER)
+        .and_then(|name| name.strip_suffix(MARKER_END))
+        .unwrap();
     (prefix, name, postfix)
 }
 
 impl<'a> Display for Archive<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut comment = Cow::Borrowed(self.comment);
-        fix_newline(&mut comment);
-        write!(f, "{}", comment)?;
+        let comment = String::from_utf8_lossy(&self.comment);
+        write!(f, "{comment}")?;
 
         for File { name, data } in &self.files {
+            let name = String::from_utf8_lossy(name);
             writeln!(f, "-- {name} --")?;
-
-            let mut data = Cow::Borrowed(*data);
-            fix_newline(&mut data);
-            write!(f, "{}", data)?;
+            let data = String::from_utf8_lossy(data);
+            write!(f, "{data}")?;
         }
 
         Ok(())
     }
 }
 
-fn fix_newline(s: &mut Cow<'_, str>) {
-    if !s.is_empty() && !s.ends_with('\n') {
-        s.to_mut().push('\n');
+fn fix_newline(s: &mut Cow<'_, [u8]>) {
+    if !s.is_empty() && !s.ends_with(&[b'\n']) {
+        s.to_mut().push(b'\n');
     }
 }
 
